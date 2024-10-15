2e21f59a0d03bf0958194f732b4a87b35e3a1afc
$NAME <$EMAIL>
2024-10-15 14:37:38 +0000

    fix: Update models, add embeddings

--- .github/workflows/update-version.sh
+++ .github/workflows/update-version.sh
@@ -1,2 +1,4 @@
 #!/bin/bash 
 sed -i "s/llama-2-7b-chat:[0-9.]*/llama-2-7b-chat:$1/" ../../README.md
+sed -i "s/llama-3.2-3b:[0-9.]*/llama-3.2-3b:$1/" ../../README.md
+sed -i "s/embeddings:[0-9.]*/embeddings:$1/" ../../README.md
